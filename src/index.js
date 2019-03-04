49f14ea130218e5163ed964624f5f2aab545cff4
Ryota Yamamoto <ryota-yamamoto@retty.me>
2019-03-04 15:18:00 +0900

    use spread operator

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const mergeStrategy = (toVal, fromVal) => {
     if (res2.script) {
       const hasScript = (res1.script || []).some(s => s.hid === res2.script[0].hid);
       if (!hasScript) {
-        res1.script = res1.script ? res1.script.concat(res2.script) : res2.script;
+        res1.script = [...(res1.script || []), res2.script];
       }
     }
     if (res2.__dangerouslyDisableSanitizersByTagID) {
