6d26b93b04baf7c6e79e8425475a2935fa81f98b
Ryota Yamamoto <ryota-yamamoto@retty.me>
2019-03-04 14:51:44 +0900

    bug fix of removing all tags in nuxt.confi.js head options

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
 const JsonldMixin = require('./mixin');
+
+const getValue = (val, context) => {
+  if (typeof val === 'object') {
+    return val
+  }
+  if (typeof val === 'function') {
+    return val.call(context)
+  }
+  return undefined
+}
+
 const mergeStrategy = (toVal, fromVal) => {
   if (!toVal) return fromVal;
   if (!fromVal) return toVal;
 
-  if (typeof fromVal === 'function' && typeof toVal === 'function') {
-    return function head() {
-      const res1 = fromVal.call(this);
-      const res2 = toVal.call(this);
-      if (res2.script) {
-        const hasScript = (res1.script || []).some(s => s.hid === res2.script[0].hid);
-        if (!hasScript) {
-          res1.script = res1.script ? res1.script.concat(res2.script) : res2.script;
-        }
-      }
-      if (res2.__dangerouslyDisableSanitizersByTagID) {
-        res1.__dangerouslyDisableSanitizersByTagID = {
-          ...(res1.__dangerouslyDisableSanitizersByTagID || {}),
-          ...res2.__dangerouslyDisableSanitizersByTagID,
-        };
-      }
+  return function head() {
+    const res1 = getValue(fromVal, this)
+    const res2 = getValue(toVal, this)
+    console.log("------------------------------------------"); // eslint-disable-line
+    console.log(res1); // eslint-disable-line
+    console.log(res2); // eslint-disable-line
 
-      return res1;
-    };
-  }
+    if (res2.script) {
+      const hasScript = (res1.script || []).some(s => s.hid === res2.script[0].hid);
+      if (!hasScript) {
+        res1.script = res1.script ? res1.script.concat(res2.script) : res2.script;
+      }
+    }
+    if (res2.__dangerouslyDisableSanitizersByTagID) {
+      res1.__dangerouslyDisableSanitizersByTagID = {
+        ...(res1.__dangerouslyDisableSanitizersByTagID || {}),
+        ...res2.__dangerouslyDisableSanitizersByTagID,
+      };
+    }
+
+    return res1;
+  };
 };
 
 module.exports = {
