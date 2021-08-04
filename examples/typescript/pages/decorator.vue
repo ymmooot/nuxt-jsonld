f0313f4b6bace7f518287b8aaeb755d8a37a8f69
ymmooot <ryota.yama.9+github@gmail.com>
2021-08-04 13:34:02 +0900

    fix: use json attribute of vue-meta to sanitize content
    https://vue-meta.nuxtjs.org/api/#json
    
    fix #671

--- examples/typescript/pages/decorator.vue
+++ examples/typescript/pages/decorator.vue
@@ -1,28 +1,26 @@
 <template>
   <div>
-    <h1>
-      typescript example
-    </h1>
-    <button @click="() => count++">+</button><br>
+    <h1>typescript example</h1>
+    <button @click="() => count++">+</button><br />
     <nuxt-link to="/">Options API</nuxt-link>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
-import { Jsonld } from 'nuxt-jsonld'
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Jsonld } from 'nuxt-jsonld';
 
 @Jsonld
 @Component
 export default class Sample extends Vue {
-  name = 'vue.js'
-  count = 0
+  name = 'vue.js';
+  count = 0;
 
   jsonld(): object {
     return {
-      '@context': "https://schema.org",
-      "@type": "Product",
+      '@context': 'https://schema.org',
+      '@type': 'Product',
       name: this.name,
       count: this.count,
     };
--- examples/typescript/pages/index.vue
+++ examples/typescript/pages/index.vue
@@ -1,36 +1,34 @@
 <template>
   <div>
-    <h1>
-      typescript example
-    </h1>
-    <button @click="() => count++">+</button><br>
+    <h1>typescript example</h1>
+    <button @click="() => count++">+</button><br />
     <nuxt-link to="/decorator">Class API</nuxt-link>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import { WithContext, Product } from 'schema-dts'
+import Vue from 'vue';
+import { WithContext, Product } from 'schema-dts';
 
 type Data = {
-  name: string,
-  count: number,
-}
+  name: string;
+  count: number;
+};
 
 export default Vue.extend({
   data(): Data {
     return {
       name: 'vue.js',
       count: 0,
-    }
+    };
   },
   jsonld(): WithContext<Product> {
     return {
-      '@context': "https://schema.org",
-      "@type": "Product",
+      '@context': 'https://schema.org',
+      '@type': 'Product',
       name: this.name,
       productID: this.count.toString(),
     };
   },
-})
+});
 </script>
