9eaf3c8a349cc1b14e2e296a089a1dd3d7a4e8f4
ymmooot <ryota.yama.9+github@gmail.com>
2021-10-25 18:40:47 +0900

    fix: fix types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
+import VueT from 'vue';
+import { MetaInfo } from 'vue-meta';
+import { Thing, WithContext } from 'schema-dts';
+
 import createJsonldMixin from './createMixin';
 import decorator from './decorator';
+
+declare module 'vue' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface ComponentOptions<V extends VueT> {
+    jsonld?: () => WithContext<Thing> | WithContext<Thing>[] | null;
+    head?: MetaInfo | (() => MetaInfo);
+  }
+}
 
 export const Jsonld = decorator;
 
