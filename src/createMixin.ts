5077cf2c9c9e2bf97e67e2fb66250c9dc07efe1e
ymmooot <ryota.yama.9+github@gmail.com>
2023-02-05 12:22:31 +0900

    fix: fix a bug that jsonld is not generated on SSR with nuxt/bridge
    fix #1085

--- src/createMixin.ts
+++ src/createMixin.ts
@@ -9,11 +9,18 @@ export default (): JsonldMixin => {
   return {
     beforeCreate(this: Vue) {
       if (this.$options && typeof this.$options.jsonld === 'function') {
-        const originalHead = this.$options.head;
+        const originalHead = this.$options.head ?? this.$options.computed?.$metaInfo;
 
         this.$options.head = () => {
           return mergeHead.call(this, originalHead);
         };
+
+        // for @nuxt/bridge
+        if (this.$options.computed) {
+          this.$options.computed.$metaInfo = () => {
+            return mergeHead.call(this, originalHead);
+          };
+        }
       }
     },
   };
