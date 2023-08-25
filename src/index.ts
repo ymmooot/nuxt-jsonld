8f54bbe0f0a2ff9853bca3dde661578d8694e70e
David Ovčačík <david.ovcacik@gmail.com>
2023-08-25 11:15:07 +0200

    Fix vue type augmentations

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Thing, Graph, WithContext } from 'schema-dts';
 import createJsonldMixin from './createMixin';
 import decorator from './decorator';
 
-declare module 'vue' {
+declare module 'vue/types/options' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ComponentOptions<V extends VueT> {
     jsonld?: () => WithContext<Thing> | WithContext<Thing>[] | Graph | null;
