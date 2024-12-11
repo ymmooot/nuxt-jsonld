822fff49c471ca6f31af9fcf48852370347df889
ymmooot <ryota.yama.9+github@gmail.com>
2024-12-11 19:16:20 +0900

    chore: update example code

--- packages/example/pages/composable-options.vue
+++ packages/example/pages/composable-options.vue
@@ -1,24 +1,21 @@
 <template>
   <div>
     <h1>Composable Options</h1>
+    <nuxt-link to="/"> Back to list </nuxt-link>
   </div>
 </template>
 
-<script lang="ts">
-export default defineComponent({
-  setup() {
-    useJsonld(
-      () => {
-        return {
-          '@context': 'https://schema.org',
-          '@type': 'WebSite',
-          name: 'nuxt-jsonld composable options',
-        };
-      },
-      {
-        tagPosition: 'bodyClose',
-      }
-    );
+<script lang="ts" setup>
+useJsonld(
+  () => {
+    return {
+      '@context': 'https://schema.org',
+      '@type': 'WebSite',
+      name: 'nuxt-jsonld composable options',
+    };
   },
-});
+  {
+    tagPosition: 'bodyClose',
+  }
+);
 </script>
